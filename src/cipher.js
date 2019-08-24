39319b00a0f8f403ce6227b6b5e61b1d0a2da7bf
Giovanna <giovanna.olivelima@gmail.com>
2019-08-24 13:06:30 -0300

    Projeto Code.Messages
    Este projeto tem como base a Cifra de César, na qual é possível
    codificar e decodificar mensagens.

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,51 @@
 window.cipher = {
-  // ... 
+  encode: encode,
+  decode: decode,
 };
 
-//só a função encode e decode;
+function encode(number, text) {
+  let messageEncoded="";
+  for (let actualLetter = 0; actualLetter < text.length; actualLetter++) {
+    const characterCode = text.charCodeAt(actualLetter);
+       
+    if ( characterCode >= 97 && characterCode <= 122) {
+      const calc =((characterCode - 97 + number)%26) + 97;
+      const encodedCharacter = String.fromCharCode(calc);
+      messageEncoded += encodedCharacter;
+    }
+       
+    else if (characterCode >= 60 && characterCode <= 90) {
+      const calc = ((characterCode - 65 + number)%26) + 65;
+      const encodedCharacter = String.fromCharCode(calc);
+      messageEncoded += encodedCharacter;
+    }
+
+    else { messageEncoded += text[actualLetter]; }
+        
+  }
+  return messageEncoded;
+}
+
+function decode (number, text) {
+  let messageDecoded="";
+  for (let actualLetter = 0; actualLetter < text.length; actualLetter++) {
+    const characterCode = text.charCodeAt(actualLetter);
+
+    if ( characterCode >= 97 && characterCode <= 122) {
+      const calc =((((characterCode - 97 - number)%26)+26)%26) + 97;
+      const decodedCharacter = String.fromCharCode(calc);
+      messageDecoded += decodedCharacter;
+    }
+        
+    else if (characterCode >= 60 && characterCode <= 90) {
+      const calc = ((((characterCode - 65 - number) %26)+26)%26) + 65;
+      const decodedCharacter = String.fromCharCode(calc);
+      messageDecoded += decodedCharacter;
+    }
+            
+    else { messageDecoded += text[actualLetter]; } 
+               
+  }
+    
+  return messageDecoded;
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-//o resto do js
+function code(event) {
+  event.preventDefault();
+  const text= document.getElementById("text").value;
+  const number= +document.getElementById("number").value;
+  const radio = document.getElementById("radioOne").checked;
+  let resultado; 
+
+  if (radio === true) {
+    resultado = window.cipher.encode(number, text);
+  }
+  else { 
+    resultado = window.cipher.decode(number, text);
+        
+  }
+  document.getElementById("answer").innerHTML = resultado;
+}
+
+let btn = document.getElementById("btn");
+btn.addEventListener("click", code);
