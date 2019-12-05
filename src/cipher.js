181090698f7cafad8f27393ace18d98fff05338e
Giovanna <giovanna.olivelima@gmail.com>
2019-12-05 00:39:35 -0300

    refactor

--- src/cipher.js
+++ src/cipher.js
@@ -9,13 +9,13 @@ function encode(number, text) {
     const characterCode = text.charCodeAt(actualLetter);
        
     if ( characterCode >= 97 && characterCode <= 122) {
-      const calc =((characterCode - 97 + number)%26) + 97;
+      const calc =((((characterCode - 97 + number)%26)+26)%26) + 97;
       const encodedCharacter = String.fromCharCode(calc);
       messageEncoded += encodedCharacter;
     }
        
     else if (characterCode >= 60 && characterCode <= 90) {
-      const calc = ((characterCode - 65 + number)%26) + 65;
+      const calc = ((((characterCode - 65 + number)%26)+26)%26) + 65;
       const encodedCharacter = String.fromCharCode(calc);
       messageEncoded += encodedCharacter;
     }
@@ -26,26 +26,6 @@ function encode(number, text) {
   return messageEncoded;
 }
 
-function decode (number, text) {
-  let messageDecoded="";
-  for (let actualLetter = 0; actualLetter < text.length; actualLetter++) {
-    const characterCode = text.charCodeAt(actualLetter);
-
-    if ( characterCode >= 97 && characterCode <= 122) {
-      const calc =((((characterCode - 97 - number)%26)+26)%26) + 97;
-      const decodedCharacter = String.fromCharCode(calc);
-      messageDecoded += decodedCharacter;
-    }
-        
-    else if (characterCode >= 60 && characterCode <= 90) {
-      const calc = ((((characterCode - 65 - number) %26)+26)%26) + 65;
-      const decodedCharacter = String.fromCharCode(calc);
-      messageDecoded += decodedCharacter;
-    }
-            
-    else { messageDecoded += text[actualLetter]; } 
-               
-  }
-    
-  return messageDecoded;
+function decode (-number, text) {
+  return encode(-number,text)
 }
